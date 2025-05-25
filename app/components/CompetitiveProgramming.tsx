5d5554fc946a770dc4c05ad936d454818d2cd08c
Gourav Dash <147884679+GariFFon@users.noreply.github.com>
2025-05-25 21:15:52 +0530

    leetcode update

--- app/components/CompetitiveProgramming.tsx
+++ app/components/CompetitiveProgramming.tsx
@@ -227,7 +227,7 @@ const CompetitiveProgramming = () => {
       stats: {
         rating: 1674,
         rank: "Top 15%",
-        solved: 323,
+        solved: 324,
         badges: ["100 Day Streak", "50 Day Streak", ]
       }
     },
